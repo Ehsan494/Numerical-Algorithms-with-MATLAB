f8ece62fac58ae17133060cd109ef8cbcc17ad50
Ehsan Shams <89010424+Ehsan494@users.noreply.github.com>
2025-03-20 04:00:11 +0200

    bisection_method.m

--- bisection_method.m
+++ bisection_method.m
@@ -1,32 +1,28 @@
-function [root, error] = bisection_method(f, a, b, tol)
+function [root, error] = bisection_method(f, a, b, tol, N)
+
     % Check if the function changes sign
     if f(a) * f(b) >= 0
         error('Invalid interval! The function must change signs.');
     end
     
-    error = abs(b - a);
+    error=tol; N=0;
     
     while error >= tol 
         c = (a + b) / 2; % Compute midpoint
         fc = f(c);
         
-        % Check if root is found
-        if abs(fc) < tol
-            root = c;
-            return;
-        end
-        
         % Update the interval
         if f(a) * fc < 0
             b = c;
-        else
+        else if f(b)*fc < 0
             a = c;
+        else
+           root=c; error=0;
+           break; 
         end
         
-        error = abs(b - a);
-        
+        error = abs(b - a)/2;
+       N=N+1; 
     end
     
-    % Return the final approximate root
-    root = (a + b) / 2;
 end
